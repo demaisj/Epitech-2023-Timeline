7ebbed6596a69c42f78dcc5670562eae6fedd018
Thomas Boda <thomas.boda@epitech.eu>
2018-11-08 10:06:36 +0100

    Add the first semester Unix System Programming (Part II) timeline

--- epitech2023-timeline.js
+++ epitech2023-timeline.js
@@ -74,17 +74,6 @@ function drawChart() {
 
     ['Unix System Programming (II)', 'my_ls', start(31, 12, 2018), end(13, 01, 2019)],
     ['Unix System Programming (II)', 'minishell 1', start(14, 01, 2019), end(03, 02, 2019)],
-
-    ['Elementary Programming in C (I)', 'BSQ', start(05, 11, 2018), end(25, 11, 2018)],
-    ['Elementary Programming in C (I)', 'Pushswap', start(26, 11, 2018), end(16, 12, 2018)],
-
-    ['Elementary Programming in C (II)', 'GetNextLine', start(17, 12, 2018), end(06, 01, 2019)],
-    ['Elementary Programming in C (II)', 'Matchstick', start(07, 01, 2019), end(27, 01, 2019)],
-
-    ['C Graphical Programming', 'MyScreenSaver', start(12, 11, 2018), end(02, 12, 2018)],
-    ['C Graphical Programming', 'MyHunter', start(12, 11, 2018), end(02, 12, 2018)],
-    ['C Graphical Programming', 'MyRadar', start(10, 12, 2018), end(20, 01, 2019)],
-    ['C Graphical Programming', 'MyRunner', start(10, 12, 2018), end(20, 01, 2018)],
   ]);
 
 
