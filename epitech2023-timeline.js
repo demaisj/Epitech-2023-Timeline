efe89e29853bdd421f978eb8946d83b306eb9b15
Thomas Boda <thomas.boda@epitech.eu>
2018-11-08 10:05:56 +0100

    Add the the first semester Unix System Programming (Part I) timeline

--- epitech2023-timeline.js
+++ epitech2023-timeline.js
@@ -68,6 +68,23 @@ function drawChart() {
     [ 'Mathematics', '103cipher', start(03, 12, 2018), end(16, 12, 2018)],
     [ 'Mathematics', '104intersection', start(17, 12, 2018), end(06, 01, 2019)],
     [ 'Mathematics', '105torus', start(07, 01, 2019), end(20, 01, 2019)],
+
+    [ 'Unix System Programming (I)', 'my_printf', start(05, 11, 2018), end(18, 11, 2018)],
+    [ 'Unix System Programming (I)', 'my_printf', start(19, 11, 2018), end(09, 12, 2018)],
+
+    ['Unix System Programming (II)', 'my_ls', start(31, 12, 2018), end(13, 01, 2019)],
+    ['Unix System Programming (II)', 'minishell 1', start(14, 01, 2019), end(03, 02, 2019)],
+
+    ['Elementary Programming in C (I)', 'BSQ', start(05, 11, 2018), end(25, 11, 2018)],
+    ['Elementary Programming in C (I)', 'Pushswap', start(26, 11, 2018), end(16, 12, 2018)],
+
+    ['Elementary Programming in C (II)', 'GetNextLine', start(17, 12, 2018), end(06, 01, 2019)],
+    ['Elementary Programming in C (II)', 'Matchstick', start(07, 01, 2019), end(27, 01, 2019)],
+
+    ['C Graphical Programming', 'MyScreenSaver', start(12, 11, 2018), end(02, 12, 2018)],
+    ['C Graphical Programming', 'MyHunter', start(12, 11, 2018), end(02, 12, 2018)],
+    ['C Graphical Programming', 'MyRadar', start(10, 12, 2018), end(20, 01, 2019)],
+    ['C Graphical Programming', 'MyRunner', start(10, 12, 2018), end(20, 01, 2018)],
   ]);
 
 
